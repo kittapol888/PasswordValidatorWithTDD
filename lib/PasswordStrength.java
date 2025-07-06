bebc62fda531017452e6af3ed6c601932cfc91a5
kittapol888 <kittapol.sa@ku.th>
2025-07-07 00:52:55 +0700

    Add files via upload

--- lib/PasswordStrength.java
+++ lib/PasswordStrength.java
@@ -1,7 +1,16 @@
 package lib;
 public enum PasswordStrength {
-    INVALID,
-    WEAK,
-    MEDIUM,
-    STRONG
+    INVALID("Password does not meet minimum requirements."),
+    WEAK("Password is too weak. Consider adding more complexity."),
+    MEDIUM("Password is of medium strength. Could be stronger."),
+    STRONG("Password is strong!");
+
+    private final String message;
+
+    PasswordStrength(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
 }
